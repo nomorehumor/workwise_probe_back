edb05893b9d5d86adc9a15e8de7aba2cdb8554de
Maxim <4andromed4@gmail.com>
2021-12-20 17:24:10 +0100

    Fixed JSON retrieval

--- blog/app/Http/Controllers/ArticleController.php
+++ blog/app/Http/Controllers/ArticleController.php
@@ -33,7 +33,7 @@ class ArticleController extends Controller
                 'author' => $article->author,
                 'creation_time' => $article->creation_time,
                 'publication_date' => $article->publication_date,
-                // 'mood' => $this->getTextMood($article->text)
+                'mood' => $this->getTextMood($article->text)
             ]);
         }
         return response()->json($articles);
@@ -65,7 +65,7 @@ class ArticleController extends Controller
 
         $article = Article::findOrFail($request->id)
                     ->update(['text' => $request->text]);
-        return response()->json($article, 201);
+        return response()->json($article, 200);
     }
 
 
@@ -75,25 +75,25 @@ class ArticleController extends Controller
     public function deleteArticle(Request $request) 
     {
         $this->validate($request, [
-            'id' => 'required'
+            'id' => 'required|integer'
         ]);
 
         $article = Article::findOrFail($request->id);
         $article->delete();
-        return response("Successful");  
+        return response("Successful");
     }
 
     public function getTextMood(string $text) {
         $curl = curl_init("https://sentim-api.herokuapp.com/api/v1/");
 
-        $headers = array(
+        $headers = [
             "Accept: application/json",
             "Content-Type: application/json",
-        );
+        ];
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         $data = <<<DATA
         {
-            "text": "Today is good weather"
+            "text": $text
         }
         DATA;
         
@@ -102,10 +102,13 @@ class ArticleController extends Controller
         //for debug only!
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         
         $resp = curl_exec($curl);
         curl_close($curl);    
-        return $resp;
+
+        $json_resp = json_decode($resp);
+        return $json_resp->result->type;
     }
 
     /**
